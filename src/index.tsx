8538d44d8618475ea41d3db8609443891c845033
Luis <luis.orihuela20@gmail.com>
2021-03-07 11:50:55 -0600

    Initial set up and esbuild transpiling configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm'
+import { useState, useRef, useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [code, setCode] = useState('')
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+  const ref = useRef<any>()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //start ESBuild
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) return
+    const result = await ref.current.transform(textAreaRef?.current?.value, {
+      loader: 'jsx',
+      target: 'es2015',
+    })
+
+    setCode(result.code)
+  }
+  return (
+    <div>
+      <textarea ref={textAreaRef}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
