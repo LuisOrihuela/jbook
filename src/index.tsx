5b05ba4a3e5ce40aaa989928f2bd799dcffedc21
Luis <luis.orihuela20@gmail.com>
2021-03-09 17:04:35 -0600

    add plugins to handle fetching modules from unpkg and adding handling css files

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import * as esbuild from 'esbuild-wasm'
 import { useState, useRef, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { fetchPlugin } from './plugins/fetch-plugin'
 
 const App = () => {
   const [code, setCode] = useState('')
-  const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+  const [input, setInput] = useState('')
   const ref = useRef<any>()
 
   //start ESBuild
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     })
   }
 
@@ -22,26 +23,21 @@ const App = () => {
 
   const onClick = async () => {
     if (!ref.current) return
-    // const result = await ref.current.transform(textAreaRef?.current?.value, {
-    //   loader: 'jsx',
-    //   target: 'es2015',
-    // })
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
       },
     })
-    // console.log(result)
     setCode(result.outputFiles[0].text)
   }
   return (
     <div>
-      <textarea ref={textAreaRef}></textarea>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
