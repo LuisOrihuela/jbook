8b498d4e0ac6fb04c5421a0986bb844d0259a25a
Luis <luis.orihuela20@gmail.com>
2021-03-16 22:44:14 -0600

    Added monaco code editor and prettier for code formatting on the in browser editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import * as esbuild from 'esbuild-wasm'
 import { useState, useRef, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
+//components
+import CodeEditor from './components/code-editor'
 
 const App = () => {
   const [input, setInput] = useState('')
@@ -62,6 +65,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue='const a = 1' onChange={value => setInput(value)} />
       <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
