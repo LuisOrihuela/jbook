443cd290f00530d3a135d1d8fe91fd9c7a40d9db
Luis <luis.orihuela20@gmail.com>
2021-04-03 11:43:34 -0600

    Added mark down editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
 
 //components
-import CodeCell from './components/code-cell'
+// import CodeCell from './components/code-cell'
+import TextEditor from './components/text-editor'
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   )
 }
