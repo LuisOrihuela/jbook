6ec427649eca683f951d7c92970d0eeb939d5d81
Luis Orihuela <luisorihuela@pop-os.localdomain>
2021-04-17 21:20:55 -0500

    Added redux configuration, cellReducer and actions

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 //components
 // import CodeCell from './components/code-cell'
-import TextEditor from './components/text-editor'
+import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
     <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
+      <Provider store={store}>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </Provider>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
